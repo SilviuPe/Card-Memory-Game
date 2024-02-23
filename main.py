7609808ce97fca7751893c4f142c05947892b63f
SilviuPe <silviupetrea60@gmail.com>
2024-02-23 09:16:27 -0800

    Added random background cards.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame 
 from values import FONT_PATH, CARD_IMAGE
 from components import Image, Pharagraph
+from random import randint
 pygame.init()
 
 
@@ -14,11 +15,11 @@ class Main:
 
 
     def home_screen(self) -> None:
-        self.card_image = Image(CARD_IMAGE,None,0,[100,300],self.screen, opacity = 100)
+
         self.play_text = Pharagraph(">> Test your memory <<", FONT_PATH, 28, ['center','center'],(255,255,255),self.screen)
         self.quit = Pharagraph(">> quit <<", FONT_PATH, 24, ['center',550],(255,255,255),self.screen)
         
-        self.image_ = pygame.image.load(CARD_IMAGE)
+        self.place_random_cards()
         while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -34,12 +35,26 @@ class Main:
             self.display_home_assets()
 
     def display_home_assets(self) -> None:
-        self.screen.fill((0,0,0))
-        self.screen.blit(self.image_,[100,100])
-        self.card_image.display_image()
+        self.screen.fill((0,0,0)) # fill the background
+
+        # Place random cards.
+        for card in self.cards_background:
+            card.display_image()
+
         self.play_text.display_text()
         self.quit.display_text()
         pygame.display.update()
 
+    def place_random_cards(self):
+        self.cards_number = 20
+        self.random_cards_pos = [[randint(0,1800),randint(0,900)] for random_int in range(self.cards_number)]
+        self.random_opacity = [randint(0,255) for i in range(self.cards_number)]
+        self.random_sizes = [randint(0,160) for i in range(self.cards_number)]
+        self.random_angles = [randint(0,360) for i in range(self.cards_number)]
+        self.cards_background = list()
+        for i in range(self.cards_number):
+            img = Image(CARD_IMAGE,self.random_sizes[i],self.random_angles[i]
+                        ,self.random_cards_pos[i],self.screen,self.random_opacity[i])
+            self.cards_background.append(img)
 
 Main((1900,1000)).home_screen()
